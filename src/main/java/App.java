e52cdbac6fc9ed1afb358d6df6790b0b6ecdca7a
Brychan Dempsey <Brych@Brychan-PC.kauripeak.co.nz>
2020-08-20 00:59:14 +1200

    Added some basic UI components

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,6 @@
+
+import javax.swing.*;
+
 /**
  * Hello world!
  */
@@ -6,10 +9,38 @@ public final class App {
     }
 
     /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
+     * Application entry-point
+     * 
+     * @param args
      */
     public static void main(String[] args) {
-        System.out.println("Hello World, from the Experimental Branch!");
+        // Not a fan of this UI mess, but unfamiliar with Swing components
+        JMenuBar menuBar = new JMenuBar();
+
+        JMenu helpMenu = new JMenu("Help");
+        JMenuItem aboutMenuItem = new JMenuItem("About");
+        aboutMenuItem.addActionListener((x) -> {
+            JLabel about_title, about_contributor, about_licence;
+            JFrame aboutFrame = new JFrame();
+            about_title = new JLabel("Text Editor");
+            about_contributor = new JLabel("Contributors:");
+            about_licence = new JLabel("");
+
+            aboutFrame.setTitle("About");
+            aboutFrame.add(about_title);
+            aboutFrame.add(about_contributor);
+            aboutFrame.add(about_licence);
+
+            aboutFrame.setSize(300, 300);
+            aboutFrame.setLayout(new BoxLayout(aboutFrame.getContentPane(), BoxLayout.PAGE_AXIS));
+            aboutFrame.setVisible(true);
+        });
+        helpMenu.add(aboutMenuItem);
+        menuBar.add(helpMenu);
+        JFrame appFrame = new JFrame();
+        appFrame.setJMenuBar(menuBar);
+        appFrame.setSize(500,400);
+        appFrame.setLayout(null);
+        appFrame.setVisible(true);
     }
 }
