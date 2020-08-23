93032409088f449183210e067b4e2cad245965be
Brychan Dempsey <Brych@Brychan-PC.kauripeak.co.nz>
2020-08-23 13:55:57 +1200

    Menu Bar elements added Rendering code moved to seperate functions File>Exit works as expected File>Print | File>Page Setup are superficially added Help>About set-up

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,15 @@
 
+import java.util.Map;
+import java.util.Random;
+
 import javax.swing.*;
+import javax.swing.event.MouseInputListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.text.AttributeSet.FontAttribute;
+
+import java.awt.*;
+import java.awt.print.*;
+import java.awt.event.*;
 
 /**
  * Hello world!
@@ -7,7 +17,8 @@ import javax.swing.*;
 public final class App {
     private App() {
     }
-
+    static Random PRNG;
+    static PrinterJob printerJob = PrinterJob.getPrinterJob();
     /**
      * Application entry-point
      * 
@@ -15,32 +26,168 @@ public final class App {
      */
     public static void main(String[] args) {
         // Not a fan of this UI mess, but unfamiliar with Swing components
-        JMenuBar menuBar = new JMenuBar();
+        PRNG = new Random();
+        JFrame mainFrame = prepMainFrame();
+        // Create the frame somewhere inwards from the top-right.
+        // Would be better to actually detect screen size though that requires a more 
+        // in-depth implementation. Realistically, the minimum screen real-estate for
+        // modern Windows is 1024*768, so at least part of the window is guaranteed to
+        // be on-screen with a limit of 500 px
+        mainFrame.setBounds(PRNG.nextInt(500), PRNG.nextInt(500), 600, 600);
+        mainFrame.setVisible(true);
+    }
 
+    /**
+     * Creates a 'main' window frame
+     * @return a frame representing the main content of the app
+     */
+    static JFrame prepMainFrame(){
+        JFrame mainFrame = new JFrame("Notepad");
+        JMenuBar menuBar = new JMenuBar();
+        /* ---- Menus ---- */
+        //#region File Menu
+        JMenu fileMenu = new JMenu("File");
+        JMenuItem newMenuItem = new JMenuItem("New");
+        JMenuItem newWindowMenuItem = new JMenuItem("New Window");
+        newWindowMenuItem.addActionListener((x) -> {
+            // Create a new frame; equal in size to our current frame
+            JFrame newWindow = prepMainFrame();
+            newWindow.setSize(mainFrame.getWidth(), mainFrame.getHeight());
+            newWindow.setVisible(true);
+        });
+        JMenuItem openMenuItem = new JMenuItem("Open");
+        openMenuItem.addActionListener((x) -> {
+            JFileChooser fileChooser = new JFileChooser();
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("Text Documents (*.txt)", "txt");
+            fileChooser.setFileFilter(filter);
+            int returnVal = fileChooser.showOpenDialog((JMenuItem)x.getSource());
+            if(returnVal == JFileChooser.APPROVE_OPTION) {
+                System.out.println("Selected file:" + fileChooser.getSelectedFile().getName());
+                /* Do file load in here */
+            }
+        });
+        JMenuItem saveMenuItem = new JMenuItem("Save");
+        JMenuItem saveAsMenuItem = new JMenuItem("Save As");
+        JMenuItem printMenuItem = new JMenuItem("Print");
+        printMenuItem.addActionListener((x) -> {
+            if (printerJob.printDialog()) {
+                try {printerJob.print();}
+                catch (PrinterException exc) {
+                    System.out.println(exc);
+                 }
+             } 
+        });
+        JMenuItem pageSetupMenuItem = new JMenuItem("Page Setup...");
+        pageSetupMenuItem.addActionListener((x) -> {
+            printerJob.pageDialog(printerJob.defaultPage());
+        });
+        JMenuItem exitMenuItem = new JMenuItem("Exit");
+        exitMenuItem.addActionListener((x) -> {
+            System.exit(0);
+        });
+        fileMenu.add(newMenuItem);
+        fileMenu.add(newWindowMenuItem);
+        fileMenu.add(openMenuItem);
+        fileMenu.add(saveMenuItem);
+        fileMenu.add(saveAsMenuItem);
+        fileMenu.addSeparator();
+        fileMenu.add(printMenuItem);
+        fileMenu.add(pageSetupMenuItem);
+        fileMenu.addSeparator();
+        fileMenu.add(exitMenuItem);
+        //#endregion
+        //#region Edit Menu
+        JMenu editMenu = new JMenu("Edit");
+        JMenuItem cutMenuItem = new JMenuItem("Cut");
+        JMenuItem copyMenuItem = new JMenuItem("Copy");
+        JMenuItem pasteMenuItem = new JMenuItem("Paste");
+
+        editMenu.add(cutMenuItem);
+        editMenu.add(copyMenuItem);
+        editMenu.add(pasteMenuItem);
+        //#endregion
+        //#region Help Menu
         JMenu helpMenu = new JMenu("Help");
         JMenuItem aboutMenuItem = new JMenuItem("About");
         aboutMenuItem.addActionListener((x) -> {
-            JLabel about_title, about_contributor, about_licence;
-            JFrame aboutFrame = new JFrame();
-            about_title = new JLabel("Text Editor");
-            about_contributor = new JLabel("Contributors:");
-            about_licence = new JLabel("");
-
-            aboutFrame.setTitle("About");
-            aboutFrame.add(about_title);
-            aboutFrame.add(about_contributor);
-            aboutFrame.add(about_licence);
-
-            aboutFrame.setSize(300, 300);
-            aboutFrame.setLayout(new BoxLayout(aboutFrame.getContentPane(), BoxLayout.PAGE_AXIS));
+            JFrame aboutFrame = aboutWindow();
             aboutFrame.setVisible(true);
         });
+
         helpMenu.add(aboutMenuItem);
+        //#endregion
+
+        menuBar.add(fileMenu);
+        menuBar.add(editMenu);
         menuBar.add(helpMenu);
-        JFrame appFrame = new JFrame();
-        appFrame.setJMenuBar(menuBar);
-        appFrame.setSize(500,400);
-        appFrame.setLayout(null);
-        appFrame.setVisible(true);
+        mainFrame.setJMenuBar(menuBar);
+        return mainFrame;
     }
+
+    static JFrame aboutWindow(){
+        JFrame aboutFrame = new JFrame("About");
+        JLabel about_title, about_contributor, about_licence, about_contributor_names;
+        about_title = new JLabel("Text Editor");
+        about_title.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 18));
+        about_contributor = new JLabel("Contributors:");
+        about_contributor.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 14));
+        // TODO add contributor names properly
+        about_contributor_names = new JLabel("Brychan Dempsey\nLachlan ");
+        // TODO add licence details
+        about_licence = new JLabel("<HTML><U>add licence name here</U></HTML>");
+        about_licence.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        about_licence.setForeground(Color.BLUE);
+        about_licence.addMouseListener(new ClickOnlyListener());
+
+        aboutFrame.setTitle("About");
+        aboutFrame.add(about_title);
+        aboutFrame.add(about_contributor);
+        aboutFrame.add(about_contributor_names);
+        aboutFrame.add(about_licence);
+
+        aboutFrame.setSize(300, 300);
+        aboutFrame.setLayout(new BoxLayout(aboutFrame.getContentPane(), BoxLayout.PAGE_AXIS));
+        return aboutFrame;
+    }
+
+    static void showLicence(String licenceName){
+        
+
+    }
+
+    static void onAppClosed(){
+
+    }
+}
+
+class ClickOnlyListener implements MouseListener{
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        JFrame licence = new JFrame("Licence Details");
+
+        licence.setVisible(true);
+        // TODO add licence details
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        return;
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        return;
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        return;
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        return;
+    }
+
 }
