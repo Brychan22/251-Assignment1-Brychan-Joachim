b8803a25130d0b3082601c2b236298f7ff261829
Brychan Dempsey <Brych@Brychan-PC.kauripeak.co.nz>
2020-08-24 22:56:32 +1200

    Added basic unit tests Gave editorWindows a default value Added UI support to Travis CI

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,16 +1,40 @@
-import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.*;
+
+import static java.time.Duration.ofMillis;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+
+import java.io.File;
+
 
 /**
  * Unit test for simple App.
  */
 class AppTest {
-    /**
-     * Rigorous Test.
-     */
+    static String expected = "A test string to load";
+    static File testFile = new File("test.txt");
+    @BeforeAll
+    static void createFiles() {
+        assertDoesNotThrow(() -> App.saveFile(testFile, expected), "Failed to create the test file");
+    }
+    @AfterAll
+    static void removeFiles() {
+        testFile.delete();
+    }
+
     @Test
-    void testApp() {
-        assertEquals(1, 1);
+    void testNewWindowSpeed(){
+        assertTimeout(ofMillis(5000), () -> {
+            App.createNewWindow(null);
+        });
+    }
+
+    @Test
+    void testTextSaveLoad() {
+        assertDoesNotThrow(() -> {
+            assertEquals(expected, App.loadFile(testFile));
+        }, "Failed loading the test file");
     }
 }
