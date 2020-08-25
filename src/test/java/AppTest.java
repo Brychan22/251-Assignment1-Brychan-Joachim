38324c9da540ccf55d11afebc9451f0a8c437245
Brychan Dempsey <Brych@Brychan-PC.kauripeak.co.nz>
2020-08-25 20:49:07 +1200

    Added testing binary save/load

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -14,14 +14,22 @@ import java.io.File;
  */
 class AppTest {
     static String expected = "A test string to load";
+    static byte[] expectedBytes = new byte[] {120, 125, 3, 5, 7, 9};
+    static byte[] expectedODT = new byte[] {};
+
     static File testFile = new File("test.txt");
+    static File testFileBinary = new File("test.bin");
+    static File testODT = new File("test.odt");
     @BeforeAll
     static void createFiles() {
         assertDoesNotThrow(() -> App.saveFile(testFile, expected), "Failed to create the test file");
+        assertDoesNotThrow(() -> App.saveFile(testFileBinary, expectedBytes), "Failed to create the test file");
     }
     @AfterAll
     static void removeFiles() {
         testFile.delete();
+        testFileBinary.delete();
+        testODT.delete();
     }
 
     @Test
@@ -35,6 +43,15 @@ class AppTest {
     void testTextSaveLoad() {
         assertDoesNotThrow(() -> {
             assertEquals(expected, App.loadFileString(testFile));
-        }, "Failed loading the test file");
+        }, "Failed loading the text test file");
+    }
+
+    @Test
+    void testBinarySaveLoad() {
+        assertDoesNotThrow(() -> {
+            for (int i=0; i < expectedBytes.length; i++){
+                assertEquals(expectedBytes[i], App.loadFileBytes(testFileBinary)[i]);
+            }            
+        }, "Failed loading the binary test file");
     }
 }
